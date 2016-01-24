b616379fca9a0fe9a7d1945b8540f9ff2002f77a
Karsten Schmidt <k@postspectacular.com>
2016-01-24 14:11:21 +0000

    fix playback

--- src/ex04/audio_play.c
+++ src/ex04/audio_play.c
@@ -3,9 +3,10 @@
 #include <math.h>
 
 #define VOLUME 70
-#define AUDIO_BUFFER_SIZE 512
+#define AUDIO_BUFFER_SIZE 256
 #define AUDIO_BUFFER_SIZE2 (AUDIO_BUFFER_SIZE / 2)
 #define AUDIO_BUFFER_SIZE4 (AUDIO_BUFFER_SIZE / 4)
+#define AUDIO_BUFFER_SIZE8 (AUDIO_BUFFER_SIZE / 8)
 
 #define SAMPLE_RATE AUDIO_FREQUENCY_44K
 #define TAU_RATE (M_TWOPI / (float)SAMPLE_RATE)
@@ -13,7 +14,11 @@
 #define HZ_TO_RAD(freq) ((freq)*TAU_RATE)
 #define RAD_TO_HZ(freq) ((freq)*INV_TAU_RATE)
 
-extern __IO PlaybackState playbackState;
+typedef enum {
+	BUFFER_OFFSET_NONE = 0, BUFFER_OFFSET_HALF, BUFFER_OFFSET_FULL
+} DMABufferState;
+
+__IO DMABufferState bufferState = BUFFER_OFFSET_NONE;
 
 typedef struct {
 	float freq;
@@ -21,23 +26,24 @@ typedef struct {
 	float amp;
 } Osc;
 
-uint16_t audioBuf[AUDIO_BUFFER_SIZE];
-uint16_t *currentPos;
+static uint8_t audioBuf[AUDIO_BUFFER_SIZE];
 
-Osc osc = { .freq = HZ_TO_RAD(440.0f), .phase = 0.0f, .amp = 1.0f };
+static Osc osc = { .freq = HZ_TO_RAD(110.0f), .phase = 0.0f, .amp = 1.0f };
+
+static void updateAudioBuffer();
+static void updateOscillator(int16_t *ptr);
 
 void demoAudioPlayback(void) {
 	BSP_LED_On(LED_GREEN);
-	memset(audioBuf, 0, AUDIO_BUFFER_SIZE * 2);
+	memset(audioBuf, 0, AUDIO_BUFFER_SIZE);
 	if (BSP_AUDIO_OUT_Init(OUTPUT_DEVICE_AUTO, VOLUME, SAMPLE_RATE) != 0) {
 		Error_Handler();
 	}
 
-	isPressed = 0;
-	currentPos = audioBuf;
-	BSP_AUDIO_OUT_Play(currentPos, AUDIO_BUFFER_SIZE2);
+	BSP_AUDIO_OUT_Play((uint16_t *)audioBuf, AUDIO_BUFFER_SIZE);
 
 	while (1) {
+		updateAudioBuffer();
 	}
 
 //	if (BSP_AUDIO_OUT_Stop(CODEC_PDWN_HW) != AUDIO_OK) {
@@ -45,28 +51,41 @@ void demoAudioPlayback(void) {
 //	}
 }
 
-void updateOscillator() {
-	for (uint16_t i = 0; i < AUDIO_BUFFER_SIZE4; i++) {
-		float y = sinf(osc->phase) * osc->amp;
-		osc->phase += osc->freq;
-		if (osc->phase >= M_TWOPI) {
-			osc->phase -= M_TWOPI;
+static void updateAudioBuffer() {
+	if (bufferState == BUFFER_OFFSET_HALF) {
+		int16_t *ptr = (int16_t*) &audioBuf;
+		updateOscillator(ptr);
+		bufferState = BUFFER_OFFSET_NONE;
+	}
+
+	if (bufferState == BUFFER_OFFSET_FULL) {
+		int16_t *ptr = (int16_t*) &audioBuf[AUDIO_BUFFER_SIZE >> 1];
+		updateOscillator(ptr);
+		bufferState = BUFFER_OFFSET_NONE;
+	}
+}
+
+static void updateOscillator(int16_t *ptr) {
+	for (uint16_t i = 0; i < AUDIO_BUFFER_SIZE8; i++) {
+		float y = sinf(osc.phase) * osc.amp;
+		osc.phase += osc.freq;
+		if (osc.phase >= M_TWOPI) {
+			osc.phase -= M_TWOPI;
 		}
 		int16_t yint = (int16_t) (y * 32767.0f);
-		*currentPos++ = yint;
-		*currentPos++ = yint;
+		*ptr++ = yint;
+		*ptr++ = yint;
 	}
 }
 
 void BSP_AUDIO_OUT_HalfTransfer_CallBack(void) {
-	updateOscillator();
+	bufferState = BUFFER_OFFSET_HALF;
 }
 
-// DMA callback: Calculates remaining file size & new position of the pointer
-void BSP_AUDIO_OUT_TransferComplete_CallBack() {
-	BSP_AUDIO_OUT_ChangeBuffer(currentPos, AUDIO_BUFFER_SIZE);
-	updateOscillator();
-	currentPos = audioBuf;
+void BSP_AUDIO_OUT_TransferComplete_CallBack(void) {
+	bufferState = BUFFER_OFFSET_FULL;
+	BSP_AUDIO_OUT_ChangeBuffer((uint16_t*) &audioBuf,
+	AUDIO_BUFFER_SIZE >> 1);
 }
 
 void BSP_AUDIO_OUT_Error_CallBack(void) {
