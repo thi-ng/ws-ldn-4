e696dcfe1cbe33f7385b6f52f1f5c927de3fa792
Karsten Schmidt <k@postspectacular.com>
2016-01-24 13:22:55 +0000

    add missing ex04 headers

---
+++ src/ex04/stm32f7xx_it.c
@@ -0,0 +1,34 @@
+#include "ex04/main.h"
+#include "ex04/stm32f7xx_it.h"
+
+extern SAI_HandleTypeDef haudio_out_sai;
+extern SAI_HandleTypeDef haudio_in_sai;
+
+void Error_Handler(void) {
+	BSP_LED_On(LED_GREEN);
+	while (1) {
+	}
+}
+
+void SysTick_Handler(void) {
+	HAL_IncTick();
+}
+
+/******************************************************************************/
+/*                 STM32F4xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (BSP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f4xx.s).                                               */
+/******************************************************************************/
+
+void EXTI15_10_IRQHandler(void) {
+	HAL_GPIO_EXTI_IRQHandler(KEY_BUTTON_PIN);
+}
+
+void AUDIO_OUT_SAIx_DMAx_IRQHandler(void) {
+	HAL_DMA_IRQHandler(haudio_out_sai.hdmatx);
+}
+
+void AUDIO_IN_SAIx_DMAx_IRQHandler(void) {
+	HAL_DMA_IRQHandler(haudio_in_sai.hdmarx);
+}
