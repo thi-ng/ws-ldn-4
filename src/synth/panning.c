1da4a1c38aa99036b938dcd71a780342a7fb23db
Karsten Schmidt <k@postspectacular.com>
2016-01-26 20:54:47 +0000

    minor update panning op

--- src/synth/panning.c
+++ src/synth/panning.c
@@ -20,14 +20,15 @@ uint8_t ct_synth_process_panning(CT_DSPNode *node, CT_DSPStack *stack,
 	CT_UNUSED(stack);
     CT_PanningState *state = (CT_PanningState *)(node->state);
     const float *src = state->src;
-    float pos = state->pos + *(state->lfo);
-    const float ampL = sqrtf(1.0f - pos) * (SQRT2 / 2);
-    const float ampR = sqrtf(pos) * (SQRT2 / 2);
+    const float pos = state->pos + *(state->lfo);
+    const float ampL = sqrtf(1.0f - pos) * (SQRT2 / 2.0f);
+    const float ampR = sqrtf(pos) * (SQRT2 / 2.0f);
     float *buf = node->buf;
     uint32_t len = AUDIO_BUFFER_SIZE;
     while (len--) {
         *buf++ = *src * ampL;
-        *buf++ = *src++ * ampR;
+        *buf++ = *src * ampR;
+        src++;
     }
     return 0;
 }
